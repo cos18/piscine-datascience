3bab5b300bd4e0cd0ae3978b72c9318ad6fd5237
Sungwoo Park <codest99@gmail.com>
2023-07-13 20:27:56 +0900

    chore: fix python2 ex01, 2

--- python-2-datatable/ex01/aff_life.py
+++ python-2-datatable/ex01/aff_life.py
@@ -2,43 +2,17 @@ import matplotlib.pyplot as plt
 from load_csv import load, convert_to_abs
 
 
-def unit_string_to_raw_string(value: str) -> str:
-    """Change unit format string to number string
-
-    Args:
-        value(str): target string
-
-    Raises:
-        TypeError: value string is not unit number
-
-    Returns: number value
-
-    """
-    unit_dict = {'k': 1000, 'M': 1000000, 'B': 1000000000}
-    result = 1
-    unit = value[-1]
-    if unit in unit_dict:
-        value = value[:-1]
-        result = unit_dict[unit]
-    try:
-        result *= float(value)
-    except ValueError:
-        raise TypeError('value string is not unit number')
-    return str(int(result))
-
 def main():
     """
-    loads the file `population_total.csv`,
-    and displays South Korea versus Japan
+    loads the file `life_expectancy_years.csv`,
+    and displays the South Korea information
     """
     try:
-        data = load(convert_to_abs("../data/population_total.csv"))
-        data = data.set_index('country').applymap(unit_string_to_raw_string).astype(int)
-
+        data = load(convert_to_abs("../data/life_expectancy_years.csv"))
+        data = data.set_index('country')
         _, ax = plt.subplots()
-        ax.set(xlabel='Year', ylabel='Population',
-               title='Population Projections')
-        ax.legend()
+        ax.set(xlabel='Year', ylabel='Life expectancy',
+               title='South Korea Life Expectancy Projections')
         ax.grid()
         data.loc['South Korea'].plot()
         plt.show()
@@ -48,3 +22,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
--- python-2-datatable/ex02/aff_pop.py
+++ python-2-datatable/ex02/aff_pop.py
@@ -2,23 +2,54 @@ import matplotlib.pyplot as plt
 from load_csv import load, convert_to_abs
 
 
+def unit_string_to_raw_string(value: str) -> str:
+    """Change unit format string to number string
+
+    Args:
+        value(str): target string
+
+    Raises:
+        TypeError: value string is not unit number
+
+    Returns: number value
+
+    """
+    unit_dict = {'k': 1000, 'M': 1000000, 'B': 1000000000}
+    result = 1
+    unit = value[-1]
+    if unit in unit_dict:
+        value = value[:-1]
+        result = unit_dict[unit]
+    try:
+        result *= float(value)
+    except ValueError:
+        raise TypeError('value string is not unit number')
+    return str(int(result))
+
 def main():
     """
-    loads the file `life_expectancy_years.csv`,
-    and displays the South Korea information
+    loads the file `population_total.csv`,
+    and displays South Korea versus Japan
     """
     try:
-        data = load(convert_to_abs("../data/life_expectancy_years.csv"))
-        data = data.set_index('country')
+        data = load(convert_to_abs("../data/population_total.csv")).T
+        data.columns = data.loc['country']
+        data = data.iloc[1:].applymap(unit_string_to_raw_string).astype(int)
+        data.index = data.index.astype(int)
+
         _, ax = plt.subplots()
-        ax.set(xlabel='Year', ylabel='Life expectancy',
-               title='South Korea Life Expectancy Projections')
+        ax.set(xlabel='Year', ylabel='Population',
+               title='Population Projections')
         ax.grid()
-        data.loc['South Korea'].plot()
+        ax.legend()
+        data.loc[data.index <= 2050, ['South Korea', 'Japan']].plot()
         plt.show()
+
+        # TODO: 라벨 타이틀 왜안나오지 legend 위치 변경해야함, y축 단위 변경 필요할까?
     except Exception as err:
         print(f"{type(err).__name__} : {err}")
 
 
 if __name__ == "__main__":
     main()
+
