601728a71b8ebbc41912b5490bbce5ab0422ca4b
Sungwoo Park <codest99@gmail.com>
2023-07-26 18:57:25 +0900

    chore: init ds1

---
+++ ds-1-warehouse/ex00/customers_table.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS public.customers;
+
+CREATE TABLE IF NOT EXISTS public.customers
+(
+    event_time TIMESTAMP with time zone,
+    event_type TEXT,
+    product_id INT,
+    price NUMERIC,
+    user_id BIGINT,
+    user_session CHAR(36)
+);
+
+select table_name
+from information_schema.tables
+where table_name like 'data_%_%'
+      and table_schema = 'public'
+order by table_name,
+         table_schema;
